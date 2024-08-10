c0440c1508da9a5471e9ed5b8ba7bf0c8e830fdf
M-Schlaepfer77 <159965073+M-Schlaepfer77@users.noreply.github.com>
2024-08-10 09:06:28 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <headaer>
+
+    </headaer>
+    <nav>
+
+    </nav>
+    <main>
+
+    </main>
+    <footer>
+      
+    </footer>
+
+    </>
+  );
+}
+
+export default App;
+<></>
